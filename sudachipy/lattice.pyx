b40fca146b5fbb5cbd8560f7123aca3c9f3184e2
Paul O'Leary McCann <polm@dampfkraft.com>
2020-04-26 16:21:35 +0900

    Use cythonspace memoryview
    Not sure this helps much by itself, but may help with other changes.

--- sudachipy/lattice.pyx
+++ sudachipy/lattice.pyx
@@ -93,11 +93,13 @@ cdef class Lattice:
         r_node.total_cost = 2 ** 30
         cdef LatticeNode l_node
         cdef int connect_cost, cost
+        cdef const short[:,:] connect_costs = self.grammar._matrix_view
+
         for l_node in self.end_lists[begin]:
             if not l_node.is_connected_to_bos:
                 continue
 
-            connect_cost = self.grammar._matrix_view[l_node.right_id, r_node.left_id]
+            connect_cost = connect_costs[l_node.right_id, r_node.left_id]
             if connect_cost == 0x7fff: # Grammar.INHIBITED_CONNECTION
                 continue
             cost = l_node.total_cost + connect_cost
